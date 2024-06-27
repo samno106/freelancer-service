9f67cc55e7733bde6f6f5366b2017291a2cee3a9
samno106 <khitsamno@gmail.com>
2024-06-27 16:02:03 +0700

    add layouts

---
+++ src/layouts/header/header.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "@/components"
+
+
+const  Header = () =>{
+    return (
+      <>
+      <Navbar/>
+      </>
+    )
+}
+
+export default Header
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,9 +1,10 @@
 import { Link, Outlet } from "react-router-dom";
+import Header from "./header/header";
 
 export default function Layout(){
     return(
         <>
-        <nav>Header</nav>
+        <Header/>
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
